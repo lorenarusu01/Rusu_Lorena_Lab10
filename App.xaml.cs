69e8b74c89a1d7dfd25eebe8bb54dd5204f3ed5e
lorenarusu01 <91947894+lorenarusu01@users.noreply.github.com>
2022-01-08 15:54:45 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using Rusu_Lorena_Lab10.Data;
+using System.IO;
+
+namespace Rusu_Lorena_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
